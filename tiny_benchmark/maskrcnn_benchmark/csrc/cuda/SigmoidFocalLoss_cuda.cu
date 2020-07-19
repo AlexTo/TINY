0f9b277c4735db0210b62d5aade39259733ad964
HN <harry.nguyen@outlook.com>
2020-07-19 22:21:17 +0800

    FIXED cuda in win32

--- tiny_benchmark/maskrcnn_benchmark/csrc/cuda/SigmoidFocalLoss_cuda.cu
+++ tiny_benchmark/maskrcnn_benchmark/csrc/cuda/SigmoidFocalLoss_cuda.cu
@@ -16,6 +16,9 @@
   for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; \
        i += blockDim.x * gridDim.x)
 
+long ceil_div3(long a, long b) { 
+  return (a + b - 1) / b; 
+}
 
 template <typename T>
 __global__ void SigmoidFocalLossForward(const int nthreads, 
@@ -117,7 +120,7 @@ at::Tensor SigmoidFocalLoss_forward_cuda(
   auto losses_size = num_samples * logits.size(1);
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  dim3 grid(std::min(THCCeilDiv(losses_size, 512L), 4096L));
+  dim3 grid(std::min(ceil_div3((int)losses_size, 512L), 4096L));
   dim3 block(512);
 
   if (losses.numel() == 0) {
@@ -161,7 +164,7 @@ at::Tensor SigmoidFocalLoss_backward_cuda(
   auto d_logits_size = num_samples * logits.size(1);
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  dim3 grid(std::min(THCCeilDiv(d_logits_size, 512L), 4096L));
+  dim3 grid(std::min(ceil_div3((long)d_logits_size, 512L), 4096L));
   dim3 block(512);
 
   if (d_logits.numel() == 0) {
